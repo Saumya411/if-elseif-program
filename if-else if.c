f2e4a79879b68df649c1b380678b0d3ad504549f
Saumya411 <71395277+Saumya411@users.noreply.github.com>
2020-09-17 12:12:11 +0530

    Created if-else if.c
    This file contains c program of if-else if

---
+++ if-else if.c
@@ -0,0 +1,21 @@
+//C program to know the grade of student on basus of marks
+#include<stdio.h>
+int main()
+{
+	int marks;
+	printf("Enter marks of student:");
+	scanf("%d",&marks);
+	if(marks>=85 && marks<=100)
+	printf("Grade A");
+	else if(marks>=70 && marks<=84)
+	printf("Grade B");
+	else if(marks>=55 && marks<=69)
+	printf("Grade C");
+	else if(marks>=40 && marks<=54)
+	printf("Grade D");
+	else
+	printf("Grade F");
+	return 0;
+}
+	
+	
